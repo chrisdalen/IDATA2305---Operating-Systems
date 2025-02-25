26d8b2adbcf66b66db9b5751d280f9e1460b7db4
Muna Ahmed <munaahmed@vpn-10-50-234-57.vpn-a.ntnu.no>
2025-02-25 17:17:57 +0100

    added methods to the movieticketclient class

--- Assigment 2/src/main/java/booking/MovieTicketClient.java
+++ Assigment 2/src/main/java/booking/MovieTicketClient.java
@@ -1,4 +1,25 @@
 package booking;
 
-public class MovieTicketClient {
+public class MovieTicketClient extends Thread {
+  private final MovieTicketServer movieTicketServer;
+  private final String customerName;
+  private final int ticketsToBook;
+
+  /**
+   * Constructs a new MovieTicketClient.
+   *
+   * @param movieTicketServer the shared MovieTicketServer instance.
+   * @param customerName the name of the customer booking the tickets.
+   * @param ticketsToBook the number of tickets to book.
+   */
+  public MovieTicketClient(MovieTicketServer movieTicketServer, String customerName, int ticketsToBook) {
+    this.movieTicketServer = movieTicketServer;
+    this.customerName = customerName;
+    this.ticketsToBook = ticketsToBook;
+  }
+
+  @Override
+  public void run() {
+    movieTicketServer.bookTicket(customerName, ticketsToBook);
+  }
 }
